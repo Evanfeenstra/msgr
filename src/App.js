55d1ae186cc2de615d3d6330f0b68206caf9679a
Evan Feenstra <evan@evanfeenstra.com>
2019-05-19 12:19:07 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextInput from './TextInput'
+import mockMessages from './mockMessages'
+import styles from './App.module.css'
+import * as firebase from "firebase/app";
+import "firebase/firestore";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class extends React.Component {
+
+  state={
+    messages:mockMessages
+  }
+
+  async componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyBAJVwrP5J4AhVKd5ijYtcTF9XMV6tIcY4",
+      authDomain: "msgr-2.firebaseapp.com",
+      projectId: "msgr-2",
+    });
+    
+    this.db = firebase.firestore();
+
+    this.db.collection("messages").onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          console.log("ADDED!!!!")
+          console.log(change.doc.data())
+          this.receive(change.doc.data())
+        }
+      })
+    })
+  }
+
+  send = (m) => {
+    this.db.collection("messages").add({
+      from:'me',
+      text:m.text
+    })
+  }
+
+  receive = (m) => {
+    if(!m.text) return
+    const messages = [...this.state.messages]
+    messages.unshift(m)
+    this.setState({messages})
+  }
+
+  render() {
+    const {messages} = this.state
+    return (
+      <div className={styles.wrap}>
+        <header className={styles.topbar}>msgr</header>
+        <div className={styles.messages}>
+          {messages && messages.map((m,i)=>{
+            return <Message m={m} key={i} />
+          })}
+        </div> 
+        <TextInput sendMessage={this.send} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const Message = ({m}) => {
+  const {img, text} = m
+  const from = Math.random()>0.5?"you":"me"
+  return <div className={styles.msg}>
+    {img && <div className={styles.imagebubble}>
+      <img alt="pic" src={img} />
+    </div>}
+    <div className={styles.textbubble} from={from}>
+      <div className={styles.actualtext}>{text}</div>
+    </div>
+  </div>
+}
+
+
+
---
+++ src/TextInput.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import styles from './TextInput.module.css'
+import {FiArrowUp} from 'react-icons/fi'
+
+export default class MessageInput extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      text:'',
+    }
+  }
+
+  onSend = () => {
+    const {text} = this.state
+    if(text){
+      this.props.sendMessage({text})
+      this.setState({text:''})
+    }
+  }
+
+  render(){
+    const {disabled} = this.props
+    const {text} = this.state
+    return (<div className={styles.wrap}>
+      <input className={styles.input} value={text}
+        onChange={(e) => this.setState({text:e.target.value})}
+      />
+      <button className={styles.sendbutton}
+        disabled={disabled}
+        onClick={this.onSend}>
+        <FiArrowUp />
+      </button>
+    </div>)
+  }
+}
